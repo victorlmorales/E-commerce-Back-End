715b18906c1a0a77d6ab51ee791adbfd305e32db
victorlmorales <victor.l.morales2020@gmail.com>
2023-04-07 20:23:29 -0500

    Added columns to Product model

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,43 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    // defining an id column
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    // defining a product_name column
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    // defining a price column
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    // defining a stock column
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      },
+    },
+    // defining a category_id column
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
